fb33ea97775dc45a4f650209dfdffb7197797010
jeelpatel17 <jeelpateldev@gmail.com>
2022-01-07 14:01:12 +0530

    learned a few things regarding ajax

--- newsWebsite/script.js
+++ newsWebsite/script.js
@@ -1,28 +1,20 @@
 // API KEY: cc11c187e57746c291439c114258704a
 
-// NEWS CARD
-/* <div class="card" style="width: 18rem">
-      <img src="..." class="card-img-top" alt="..." />
-      <div class="card-body">
-        <h5 class="card-title">News title</h5>
-        <p class="card-text">News Description</p>
-        <a href="#" class="btn btn-primary">Read Full News</a>
-      </div>
-    </div> */
+// let fetchBtn = document.getElementById("fetchBtn");
+// fetchBtn.addEventListener("click", fetchNews());
 
-let fetchBtn = document.getElementById("fetchBtn");
-fetchBtn.addEventListener("click", fetchNews());
+fetchNews();
 
 function fetchNews() {
-  // return new Promise(function (resolve, reject) {
   const xhr = new XMLHttpRequest();
-
+  const api = "cc11c187e57746c291439c114258704a";
   xhr.open(
     "GET",
-    `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=cc11c187e57746c291439c114258704a`,
+    `https://newsapi.org/v2/everything?q=covid&apiKey=${api}`,
     true
   );
 
+  // OPTIONAL
   xhr.onprogress = function () {
     console.log("loading...");
   };
@@ -31,29 +23,24 @@ function fetchNews() {
     e.preventDefault();
     if (this.status === 200) {
       let rawData = JSON.parse(this.responseText);
-      console.log(rawData);
+      // console.log(rawData.articles[1]);
       for (let i = 0; i < 10; i++) {
         let data = rawData.articles[i];
         console.log(data);
-        console.log(data.urlToImage);
-        console.log(data.title);
         let parent = document.getElementById("parent");
         parent.innerHTML += `
-        <div class="card">
+        <div class="card my-3 mx-auto">
       <img src="${data.urlToImage}" class="card-img-top" alt="..." />
       <div class="card-body">
-        <h5 class="card-title">${data.title}</h5>
+        <h5 class="card-title">${data.title} <span class="badge rounded-pill bg-secondary">${data.source.name}</span></h5>
         <p class="card-text">${data.description}</p>
         <a href="${data.url}" class="btn btn-primary">Read Full News</a>
       </div>
     </div>`;
       }
-      // resolve(this.responseText);
     } else {
       console.log("SOMETHING WRONG!");
-      //   reject();
     }
   };
   xhr.send();
 }
-// }
