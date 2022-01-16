d47865b3907c92592caa66fc5b91f9b00685fcf4
jeelpatel17 <jeelpateldev@gmail.com>
2022-01-16 15:17:52 +0530

    increased the width of the 0 button to match it with the iOS user interface, and made the All Clear button functional

--- appleCalculator/script.js
+++ appleCalculator/script.js
@@ -2,13 +2,14 @@ let numbers = document.getElementsByClassName("no");
 let operators = document.getElementsByClassName("op");
 let equals = document.getElementById("equals");
 let inpBar = document.getElementById("inp").value;
-let opChoice, n1, n2;
+let opChoice, opCircle, n1, n2;
 let allClearBtn = document.getElementById("allClear");
 
 // HIGHLIGHTING OPERATORS AND CLEARING INPUT ONCE OPERATOR IS SELECTED
 for (elem of operators) {
   elem.addEventListener("click", (e) => {
     e.target.classList += " activeOp";
+    opCircle = e.target;
     opChoice = e.target.value;
     n1 = document.getElementById("inp").value;
     document.getElementById("inp").value = "";
@@ -25,7 +26,10 @@ for (number of numbers) {
 equals.addEventListener("click", function arith() {
   n2 = document.getElementById("inp").value;
   let res = `${n1} ${opChoice} ${n2}`;
+  // eval() takes string in, and converts into a statement which can be arithmetically executed
   document.getElementById("inp").value = eval(res);
+  opCircle.classList -= "activeOp";
+  opCircle.classList += " rightMost";
 });
 
 allClearBtn.addEventListener("click", () => {
